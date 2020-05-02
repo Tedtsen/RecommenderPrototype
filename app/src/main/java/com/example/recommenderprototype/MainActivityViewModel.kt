b5d2dc73fb12abbf26ac8ea99719a3a3ec37e744
Tedtsen <ttw06090@gmail.com>
2020-05-02 20:32:34 +0800

    Third Commit Working user-based CF, but not yet combined with existing algorithm CF results are stored in prediction array and the algorithm works as described in the textbook, while using the same input data from textbook

--- app/src/main/java/com/example/recommenderprototype/MainActivityViewModel.kt
+++ app/src/main/java/com/example/recommenderprototype/MainActivityViewModel.kt
@@ -109,43 +109,85 @@ class MainActivityViewModel : ViewModel() {
 
                                 // //Collab - Filter
                                 //Firestore database read user-item matrix
-                                /*var userItemMatrix = mutableListOf<MutableList<Float>>()
+                                var userItemMatrix = mutableListOf<MutableList<Float>>()
                                 var currentUserRow = mutableListOf<Float>()
                                 //To store prediction of current user to all food in menu
-                                var prediction  = mutableListOf<Float>()
 
                                 odb.collection("user_item_matrix").get()
                                     .addOnSuccessListener { results->
                                         for (document in results){
+                                            //Log.d("prediction", document["CF_score"].toString())
                                             if (document.id != currentUser.email)
-                                                userItemMatrix.add(document.toString().split(",").map{it.toFloat()}.toMutableList())
-                                            else currentUserRow = document.toString().split(",").map{it.toFloat()}.toMutableList()
+                                                userItemMatrix.add(document["CF_score"].toString().split(",").map{it.toFloat()}.toMutableList())
+                                            else currentUserRow = document["CF_score"].toString().split(",").map{it.toFloat()}.toMutableList()
                                         }
 
-                                        val ramean  = currentUserRow.average()
-                                        //Columns: (rai-ramean) | (rai-ramean)^2 Calculate this first, as it is reusable
-                                        var calcTableA = mutableListOf<MutableList<Float>>()
+                                        //Calculate usera average of nonzero elements
+                                        val currentUserRowNonZero = currentUserRow.filter { it > 0F }
+                                        val RAmean  = currentUserRowNonZero.average()
+                                        //Log.d("prediction", "mean of A is " + RAmean)
+
+                                        //Columns: (rai-ramean) | (rai-ramean)^2
+                                        var calcTableA = mutableListOf(mutableListOf<Float>(),mutableListOf<Float>())
                                         for (i in 0 until menu.size){
-                                            var value : Float = (currentUserRow[i] - ramean).toFloat()
+                                            //As we need to find co-rated items, non-rated are labelled -100
+                                            var value = -100F
+                                            if (currentUserRow[i] > 0F) {
+                                                value = (currentUserRow[i]-RAmean).toFloat()
+                                            }
                                             calcTableA[0].add(value)
-                                            calcTableA[1].add(value.pow(2))
+                                            //Log.d("prediction", "calcTableA[0]["+i+"] " + value)
+                                            calcTableA[1].add(if (value > -50F) value.pow(2) else value)
                                         }
 
-                                        userItemMatrix.forEachIndexed { index, user ->
-                                            //For every other user in the matrix, calculate its table
-                                            val rbmean = user.average()
-                                            //Columns: (rbi-rbmean) | (rbi-rbmean)^2
-                                            var calcTableB = mutableListOf<MutableList<Float>>()
-                                            for (i in 0 until menu.size){
-                                                var value : Float = (user[i] - rbmean).toFloat()
-                                                calcTableB[0].add(value)
-                                                calcTableB[1].add(value.pow(2))
+
+                                        var prediction  = arrayOfNulls<Float>(size = menu.size)
+                                        for (i in 0 until menu.size) {
+                                            //Prediction equation top & bottom
+                                            var predTop = 0F
+                                            var predBtm = 0F
+                                            userItemMatrix.forEachIndexed { index, nextUser ->
+
+                                                //For every other user in the matrix, calculate its mean and table
+                                                val nextUserNonZero = nextUser.filter { it > 0F }
+                                                val RBmean = nextUserNonZero.average()
+
+                                                //Columns: (rbi-rbmean) | (rbi-rbmean)^2
+                                                var calcTableB = mutableListOf(mutableListOf<Float>(),mutableListOf<Float>())
+                                                for (i in 0 until menu.size) {
+                                                    //As we need to find co-rated items, non-rated are labelled -100
+                                                    var value = -100F
+                                                    if (nextUser[i] > 0F) {
+                                                        value = (nextUser[i] - RBmean).toFloat()
+                                                    }
+                                                    calcTableB[0].add(value)
+                                                    //Log.d("prediction", "calcTableB[0]["+i+"] " + value)
+                                                    calcTableB[1].add(
+                                                        if (value > -50F) value.pow(
+                                                            2
+                                                        ) else value
+                                                    )
+                                                }
+
+                                                //If current user has NO rating for ith item
+                                                //& next user HAS rating for ith item then do prediction
+                                                if (currentUserRow[i] <= 0F && nextUser[i] > 0F) {
+                                                    //Do prediction based on next user only is similarity is high
+                                                    if (sim(calcTableA, calcTableB) >= 0.6) {
+                                                        val simOfAB = sim(calcTableA, calcTableB)
+                                                        predTop += (simOfAB * (nextUser[i] - RBmean)).toFloat()
+                                                        predBtm += simOfAB
+                                                        //Log.d("prediction", "simofA"+index + " " + simOfAB + ", " + RBmean)
+                                                    }
+                                                }
                                             }
-                                            for (i in 0 until menu.size){
-                                                if (sim )
-                                            }
+                                            //only update prediction for current user non-rated items (<= 0)
+                                            if (currentUserRow[i] <= 0){
+                                                prediction[i] = (RAmean + predTop/predBtm).toFloat()
+                                                Log.d("prediction", prediction[i].toString() + " " + i)}
+                                            //else Log.d("prediction", currentUserRow.to)
                                         }
-                                    }*/
+                                    }
 
                                 // //Recommendation
                                 for (i in 0 until menuRecommended.size){
@@ -203,12 +245,23 @@ class MainActivityViewModel : ViewModel() {
 
     fun sim(userA : MutableList<MutableList<Float>> , userB : MutableList<MutableList<Float>>)  : Float {
         var top = 0F
-        var btm = 0F
+        var btmA = 0F
+        var btmB = 0F
         for (i in 0 until userA[0].size)
         {
-            top += userA[0][i]*userB[0][i]
-            btm += userB[1][i]*userB[1][i]
+            //Check if greater than -50F, if lesser than -50F then it is (non-rated)
+            //If userA ith item is rated && userB as well, calculate top and bottom of equation
+            if (userA[0][i] > -50F && userB[0][i] > -50F) {
+                top += userA[0][i] * userB[0][i]
+                //Log.d("prediction", "userA[0]["+i+"] " + userA[0][i])
+                //Log.d("prediction", "userB[0]["+i+"] " + userB[0][i])
+                btmA += userA[1][i]
+                btmB += userB[1][i]
+                //Log.d("prediction", "userA[1]["+i+"] " + userA[1][i])
+                //Log.d("prediction", "userB[1]["+i+"] " + userB[1][i])
+                //Log.d("prediction", "top " + top + " bottom " + btmA*btmB)
+            }
         }
-        return top/sqrt(btm)
+        return top/sqrt(btmA*btmB)
     }
 }
