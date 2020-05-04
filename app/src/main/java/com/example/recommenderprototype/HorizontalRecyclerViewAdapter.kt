6000d4cab791b31bef2311aed90f41fa6d0b973d
Tedtsen <ttw06090@gmail.com>
2020-05-04 18:46:23 +0800

    Sixth Commit Fixed a bug where when a category element is clicked, it is not highlighted and selected

--- app/src/main/java/com/example/recommenderprototype/HorizontalRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/recommenderprototype/HorizontalRecyclerViewAdapter.kt
@@ -29,33 +29,34 @@ class HorizontalRecyclerViewAdapter(val categories: List<String>, foodGridRecycl
     override fun getItemCount() = categories.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val categoriesStrings = holder.itemView.resources.getStringArray(R.array.categories).toList()
         holder.categoryName.text = categories[position]
         holder.itemView.setOnClickListener {
-            if (categories[position] == "Popular") {
+            if (categories[position] == categoriesStrings[0]) {
                 changeRecyclerViewList(holder, 0, position)
             }
-            else if (categories[position] == "Suggestions") {
+            else if (categories[position] == categoriesStrings[1]) {
                 changeRecyclerViewList(holder, 1, position)
             }
-            else if (categories[position] == "Rice") {
+            else if (categories[position] == categoriesStrings[2]) {
                 changeRecyclerViewList(holder, 2, position)
             }
-            else if (categories[position] == "Noodle") {
+            else if (categories[position] == categoriesStrings[3]) {
                 changeRecyclerViewList(holder, 3, position)
             }
-            else if (categories[position] == "Dumpling") {
+            else if (categories[position] == categoriesStrings[4]) {
                 changeRecyclerViewList(holder, 4, position)
             }
-            else if (categories[position] == "Biscuit") {
+            else if (categories[position] == categoriesStrings[5]) {
                 changeRecyclerViewList(holder, 5, position)
             }
-            else if (categories[position] == "Soup") {
+            else if (categories[position] == categoriesStrings[6]) {
                 changeRecyclerViewList(holder, 6, position)
             }
-            else if (categories[position] == "Bread") {
+            else if (categories[position] == categoriesStrings[7]) {
                 changeRecyclerViewList(holder, 7, position)
             }
-            else if (categories[position] == "Other") {
+            else if (categories[position] == categoriesStrings[8]) {
                 changeRecyclerViewList(holder, 8, position)
             }
         }
